19e4fd6529c35a45445203e2c2e7b0bfda39a749
kacan98 <kacan@dynaway.com>
2023-10-25 00:13:49 +0200

    Fix small typos

--- backend/SQL files/Tables/User.sql
+++ backend/SQL files/Tables/User.sql
@@ -6,6 +6,6 @@ CREATE TABLE R8titSchema.Users
     , ImageId INT
     , Active BIT
     , Username NVARCHAR(50) NOT NULL
-    , Email NVARCHAR(50) NOT NULL,
+    , Email NVARCHAR(50) NOT NULL
     , AdminRights BIT
 );

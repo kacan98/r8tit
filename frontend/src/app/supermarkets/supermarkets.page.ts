d6162a1e7be6b6371446e97526cde4e1ac682b79
kacan98 <kacan@dynaway.com>
2023-11-05 17:07:27 +0100

    Rename first tab to supermarkets

---
+++ frontend/src/app/supermarkets/supermarkets.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'supermarkets.page.html',
+  styleUrls: ['supermarkets.page.scss']
+})
+export class SupermarketsPage {
+
+  constructor() {}
+
+}
